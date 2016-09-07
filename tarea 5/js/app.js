b5c4cc1f620535ddbe422aa66701f59191aadcd2
NewtonJoelMiranda <newtom.miranda@hotmail.com>
2016-09-06 21:58:27 -0400

    Tarea 4 y 5

---
+++ tarea 5/js/app.js
@@ -0,0 +1,28 @@
+(function (){
+console.log('Holaaaa que tal Mundo');
+
+
+//variables array
+var lista = [];
+
+lista.push('Que tal');
+lista.push('Mundo');
+
+
+console.log(lista);
+
+//variables tipo String
+var str= 'Hola';
+
+//variables tipo numerico
+var numerico= 120;
+
+//variable del tipo object
+var object ={};
+
+object.descripcion ='Apples';
+object.cantidad = 10;
+console.log(object);
+
+
+}());
